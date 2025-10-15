0d90143643682e3f3d992cd2a96cd91c8da87349
Antonio Navarro <dojeDulDev@Proton.me>
2025-10-15 13:56:43 +0200

    feat: define user model

--- server.ts
+++ server.ts
@@ -3,13 +3,13 @@ import express from 'express';
 // Carga las variables de entorno. Se pueden usar en todo el proyecto. 
 import 'dotenv/config'
 // Carga la conexion a la bbdd
-// import {connectDB} from './config/bd'
+import {connectDB} from './config/bd'
 import cors from 'cors';
 import {corsConfig} from './config/cors'
 import helmet from 'helmet';
 const app = express()
 
-// connectDB();
+connectDB();
 app.use(cors(corsConfig))
 app.use(helmet())
 // Permite leer los datos que vienen de un formulario
