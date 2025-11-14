16d7fc330bdda319bae66bb618b8e28b3eb38023
Antonio Navarro <dojeDulDev@Proton.me>
2025-11-14 16:36:47 +0100

    feat login

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,11 +1,41 @@
 import { Response, Request, NextFunction } from "express";
+import {validateLogin} from '../utils/validator'
+import * as AuthServices from '@services/user.service'
 
-export const login = async (req: Request, res: Response, next: NextFunction) => {
+export const login = async (req: Request, res: Response, next: NextFunction) => {       
+
+    try {
+        
+        const {email, password} = req.body
+
+        const validUser = validateLogin(req.body)
+
+        if (!validUser.valid) {
+            res.status(400).json({
+                message: "Datos del rgistro no validos", 
+                errors: validUser.errors
+            })
+        }
+
+        const userLogged = AuthServices.login(req.body)
+
+        res.status(200).json({userLogged})
+
+        
+
+    } catch (error) {
+        next(error)
+    }
 
 
-    const {email, password} = req.body
+
+
 
     
 
 
+}
+
+export const register = async (req: Request, res: Response, next: NextFunction) => { 
+    
 }
