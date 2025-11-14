fafb00d9a05787c25e90aee0735e7d0145088901
Antonio Navarro <dojeDulDev@Proton.me>
2025-11-14 09:40:09 +0100

    feat: create an endpoint to get all tasks

--- src/services/task.services.ts
+++ src/services/task.services.ts
@@ -1,11 +1,17 @@
 import { Request, Response, NextFunction } from "express"
+import AppError from "../errors/AppError"
+import logger from '../utils/logger'
+import taskModel from '@models/task.model'
 
 export const getAllTask = async () => {
 
     try {
-        
+        return await taskModel.find({})
     } catch (error) {
-        
+       const errorMessage = error instanceof Error ? error.message : String(error)
+       logger.error(errorMessage) 
+       throw AppError.unexpected('error al obtener las tareas')
     }
     
 }
+
